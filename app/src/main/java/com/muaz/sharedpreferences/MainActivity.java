f479030f0fd32e7d4941d6c2fb479fb7c4d7fc17
muaz <ahmedmuaz0152@gmail.com>
2019-06-30 17:22:33 +0800

    fix to show all value in listview

--- app/src/main/java/com/muaz/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/muaz/sharedpreferences/MainActivity.java
@@ -1,5 +1,6 @@
 package com.muaz.sharedpreferences;
 
+import android.os.Build;
 import android.os.StrictMode;
 import android.support.annotation.RequiresPermission;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -20,6 +22,7 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     ListView display;
@@ -42,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (android.os.Build.VERSION.SDK_INT > 9)
+        if (Build.VERSION.SDK_INT > 9)
         {
             StrictMode.ThreadPolicy policy = new
                     StrictMode.ThreadPolicy.Builder().permitAll().build();
@@ -56,14 +59,21 @@ public class MainActivity extends AppCompatActivity {
             InputStream stream = new URL(url).openStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
             json_result = readData(reader);
-            Log.i("jsonresult", json_result);
-             object = new JSONObject(json_result);
+            JSONObject json_data = new JSONObject(json_result);
+                data.add(json_data.getString("activity"));
+                data.add(json_data.getString("accessibility"));
+                data.add(json_data.getString("type"));
+                data.add(json_data.getString("participants"));
+                data.add(json_data.getString("price"));
+                data.add(json_data.getString("link"));
+                data.add(json_data.getString("key"));
+            Log.i("data", data.toString());
         } catch (IOException e) {
             e.printStackTrace();
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        data.add(json_result);
+//        data.add(json_result);
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,  data);
         display.setAdapter(adapter);
     }
